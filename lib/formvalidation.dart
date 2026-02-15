a33769ad0e9c7019d455f5a5d2c9e4e62ac16b0e
Aman Singh <xaman282@gmail.com>
2026-02-16 00:14:40 +0530

    form validation

--- lib/formvalidation.dart
+++ lib/formvalidation.dart
@@ -8,62 +8,130 @@ class Form_Validation extends StatefulWidget {
 }
 
 class _Form_ValidationState extends State<Form_Validation> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Form Validation"), centerTitle: true),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Your Name",
-                labelText: "Name",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                prefixIcon: Icon(Icons.person)
-              ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                hintText: "Enter your Phone NUmber",
-                labelText: "Phone Number",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                prefixIcon: Icon(Icons.phone)
 
-              ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Enter your Email",
-                labelText: "Email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  hintText: "Enter Your Name",
+                  labelText: "Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: Icon(Icons.person)
                 ),
-                prefixIcon: Icon(Icons.email)
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please Enter Your Name";
+                  }
+                  return null;
+                }
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter password",
-                labelText: "password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+
+              SizedBox(height: 15),
+
+              TextFormField(
+                controller: phoneController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter your Phone Number",
+                  labelText: "Phone Number",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: Icon(Icons.phone)
+
                 ),
-                prefixIcon: Icon(Icons.lock)
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please Enter Your Number";
+                    }
+                    return null;
+                  }
               ),
-            ),
-          ],
+
+              SizedBox(height: 15),
+
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Enter your Email",
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: Icon(Icons.email)
+                ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please Enter Your Email";
+                    }
+                    else if (!value.contains("@")){
+                      return "Please Enter Valid Email";
+                    }
+                    return null;
+                  }
+              ),
+
+              SizedBox(height: 15),
+
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  labelText: "password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: Icon(Icons.lock)
+                ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please Enter Your Password";
+                    }
+                    return null;
+                  }
+              ),
+
+              SizedBox(height: 15),
+
+
+
+              ElevatedButton(onPressed: (){
+                       if(_formKey.currentState!.validate()){
+                         print("Name: ${nameController.text}");
+                         print("Phone: ${phoneController.text}");
+                         print("Email: ${emailController.text}");
+                         print("Password: ${passwordController.text}");
+                       }
+
+              }, child: Text("Submit",style: TextStyle(color: Colors.white),),style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(15),
+              ),
+              ),
+            ],
+          ),
         ),
       ),
     );
